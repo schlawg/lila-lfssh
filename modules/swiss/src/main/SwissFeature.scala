5bda6ebcdbf159567ee7d24c5e5757c3b8f39bc3
Jonathan Gamble <101470903+schlawg@users.noreply.github.com>
2023-06-13 10:50:46 -0500

    remove hack

--- modules/swiss/src/main/SwissFeature.scala
+++ modules/swiss/src/main/SwissFeature.scala
@@ -38,8 +38,6 @@ final class SwissFeature(
   private val startsAtOrdering = Ordering.by[Swiss, Long](_.startsAt.toMillis)
 
   private def getForTeams(teams: Seq[TeamId]): Fu[FeaturedSwisses] =
-    if mode != play.api.Mode.Prod then teams map swissCache.featuredInTeam.invalidate
-
     teams.map(swissCache.featuredInTeam.get).parallel.dmap(_.flatten) flatMap { ids =>
       mongo.swiss.byIds[Swiss, SwissId](ids, ReadPreference.secondaryPreferred)
     } map {
